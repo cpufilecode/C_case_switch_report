9db1c767b4e02935285c88b4167db73e4d803a77
cpufilecode <zf0908@naver.com>
2023-03-29 14:54:12 +0900

    upload

--- report.c
+++ report.c
@@ -4,5 +4,25 @@
 void main()
 {
 	int num = 0;
+	printf("점수를 입력해주세요. \n");
+	scanf("%d", &num);
 
+	switch (num / 10)
+	{
+	case 10: case 9:
+		printf("A학점 입니다. \n");
+		break;
+	case 8:
+		printf("B학점 입니다. \n");
+		break;
+	case 7:
+		printf("C학점 입니다. \n");
+		break;
+	case 6:
+		printf("D학점 입니다. \n");
+		break;
+	default:
+		printf("F학점 입니다. \n");
+		break;
+	}
 }
